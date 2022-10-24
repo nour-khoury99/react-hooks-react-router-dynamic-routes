17bbaf5487a9d77342313acaca2875d3097e1d14
nour khoury <khourynour79@gmail.com>
2022-10-24 10:23:39 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import App from "./components/App";
+import { Route, useRouteMatch } from "react-router-dom";
+import MoviesList from "./MoviesList";
+import MovieShow from "./MovieShow";
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById("root")
-);
+function MoviesPage({ movies }) {
+  const match = useRouteMatch();
+
+  return (
+    <div>
+      <MoviesList movies={movies} />
+
+   
+      <Route exact path={match.url}>
+        <h3>Choose a movie from the list above</h3>
+      </Route>
+
+      <Route path={`${match.url}/:movieId`}>
+        <MovieShow movies={movies} />
+      </Route>
+    </div>
+  );
+}
+
+export default MoviesPage;
